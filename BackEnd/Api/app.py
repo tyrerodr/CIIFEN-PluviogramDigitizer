ccae4b2f9877929fe6be6465d418605629196395
Tyrone-PC <tyrerodr@espol.edu.ec>
2022-09-16 15:21:07 -0500

    Cambios modelo

--- BackEnd/Api/app.py
+++ BackEnd/Api/app.py
@@ -107,7 +107,7 @@ def obtener_estaciones():
 def obtener_pluviogramasModelo():
     # mydb=mysql.connector.connect(host="localhost",user="root",passwd="",database="ciifen",auth_plugin='mysql_native_password')
     cur = mydb.cursor()
-    cur.execute('''SELECT distinct(modelo) FROM pluviograma''')
+    cur.execute('''SELECT nombre FROM modelo''')
     results = cur.fetchall()
     response = jsonify(results)
     return response
